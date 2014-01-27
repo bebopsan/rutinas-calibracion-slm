45632d7b06fc52fd3cebae0e951bf0f398b48e1d
bebopsan <santiag77e@gmail.com>
2014-01-27 11:02:49 -0500

    Se realizaron de nuevo las 10 medidas básicas usando los cambios que se hicieron a los estados circulares según la tabla de estados de enero.
    En este punto hace falta correr el archivo de ipython para analizar el resultado.

--- MED_CORR_FASE_vrs_NG.m
+++ MED_CORR_FASE_vrs_NG.m
@@ -88,8 +88,8 @@ nBands = get(vid, 'NumberOfBands');
  h = fspecial('average',[9 9]);
 for rep=1:Nmed%:10
     
-    cont=256;
-for n=1:1:256 % De aquí se cambia cada cuanto se toman imágenes.
+    cont=52;
+for n=1:5:256 % De aquí se cambia cada cuanto se toman imágenes.
     NG=256-n;
     fprintf('Nivel de gris %g \n',NG)
     recuadro=ones(tamV,tamH).*255; recuadro(tamV/2:end,:)=NG; %elemento a proyectar
@@ -111,9 +111,10 @@ drawnow
     imaref=F(f1:f2,c1:c2);
     imaref=imfilter(mat2gray(imaref),h,'replicate');
     imaref=mat2gray(imaref);
-    imshow(imaref);
     
-    imwrite( imaref, strcat( 'imaref_' , num2str(n) ,'.png'));
+    %imshow(imaref);
+    
+    %imwrite( imaref, strcat( 'imaref_' , num2str(n) ,'.png'));
     
     %save('imaref' + num2str(n) , uint8(imaref) );
     
@@ -121,9 +122,11 @@ drawnow
     imacorr=F(f3:f4,c1:c2);clear ima
     imacorr=imfilter(mat2gray(imacorr),h,'replicate');
     imacorr=mat2gray(imacorr);
-    imshow(imacorr);
-    imwrite( imacorr, strcat( 'imacorr_' , num2str(n) ,'.png'));
-    pause(1); 
+    %imshow(imacorr);
+    
+    %imwrite( imacorr, strcat( 'imacorr_' , num2str(n) ,'.png'));
+    %pause(2); 
+    
     %save('imacorr' + num2str(n) , imacorr )
     %imshow(imaref);axis on
 
